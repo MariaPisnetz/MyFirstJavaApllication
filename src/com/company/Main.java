fec6ab84262c4ae4efa5f8577ee52a4a1b25f030
Maria Pisnetz <mariapisnetz@gmail.com>
2021-10-26 10:20:50 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System system;
+      System.out.println("This is my first java application");
+    }
+}
