42e7d491debf20a2d56b363125a40520f7862c82
Maria Pisnetz <mariapisnetz@gmail.com>
2021-10-26 10:27:48 +0300

    First Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System system;
-      System.out.println("This is my first java application-change- change 2");
+      System.out.println("This is my first java application-change- change 2 -change 3 ");
     }
 }
